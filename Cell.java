e95faa20741a79e6e7acf807aae372ec249b94c1
Brian Schott <bschott@cgps.org>
2024-02-14 09:23:45 -0500

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,36 @@
+public class Cell {
+
+  //constants for ease of changing out what is stored in each spot
+  private final String EMOJI = "🦠";
+  private final String EMPTY = "🔲";
+  //boardSpace is the current status of each space, whether it be an EMOJI or EMPTY
+  private String boardSpace;
+  private boolean isAlive;
+  
+  public Cell(boolean isAlive) {
+    setIsAlive(isAlive);
+    
+  }
+
+  public void setIsAlive(boolean isAlive) {
+    this.isAlive = isAlive;
+    setEmoji();
+  }
+
+  public boolean getIsAlive() {
+    return isAlive;
+  }
+
+  private void setEmoji() {
+    if (this.isAlive) {
+      boardSpace = EMOJI;
+    }
+    else {
+      boardSpace = EMPTY;
+    }
+  }
+
+  public String toString() {
+    return boardSpace;
+  }
+}
